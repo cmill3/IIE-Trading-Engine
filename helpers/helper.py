c396415bba3f38b6a379708e2f3060d42c56a3e4
Charles <cfmiller1397@gmail.com>
2023-09-07 10:05:00 -0400

    add bfC/bfP

--- helpers/helper.py
+++ helpers/helper.py
@@ -114,9 +114,9 @@ def calculate_floor_pct(row):
    low_price = trimmed_df['l'].min()
    if len(trimmed_df) == 0:
        return float(row['underlying_purchase_price'])
-   if row['trading_strategy'] in ['maP', 'day_losers','vdiff_gainP']:
+   if row['trading_strategy'] in ['maP', 'day_losers','vdiff_gainP','bfP']:
        return low_price
-   elif row['trading_strategy'] in ['most_actives', 'day_gainers','vdiff_gainC']:
+   elif row['trading_strategy'] in ['most_actives', 'day_gainers','vdiff_gainC','bfC']:
        return high_price
    else:
         return 0
