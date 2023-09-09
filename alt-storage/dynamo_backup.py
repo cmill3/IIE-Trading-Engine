9753b15357340f56887a5041975d73d1dc486efb
dkedir <kedirdean@gmail.com>
2023-09-09 07:44:29 -0400

    Updated to delete all but the most recent 2 backups

--- alt-storage/dynamo_backup.py
+++ alt-storage/dynamo_backup.py
@@ -8,52 +8,34 @@ ddbRegion = os.environ['AWS_DEFAULT_REGION']
 table_list = os.environ['TABLE_LIST']
 table_list = ast.literal_eval(table_list)
 current_date_and_time = datetime.now()
-backupName = 'scheduled_backup_' + str(current_date_and_time.year) + "_" + str(current_date_and_time.month) + "_" + str(current_date_and_time.day) + "_" + str(current_date_and_time.hour) + "_" + str(current_date_and_time.minute)
-print('Backup started for: ', backupName)
+backup_name = 'scheduled_backup_' + str(current_date_and_time.year) + "_" + str(current_date_and_time.month) + "_" + str(current_date_and_time.day) + "_" + str(current_date_and_time.hour) + "_" + str(current_date_and_time.minute)
+print('Backup started for: ', backup_name)
 ddb = boto3.client('dynamodb', region_name=ddbRegion)
  
 def lambda_handler(event, context):
     try:
         for item in table_list:
-        #create backup
-            ddb.create_backup(TableName=item, BackupName = backupName)
-            print('Backup has been taken successfully for table:', item)
-            
-            #check number/information of backup in the set time range (backups older than 3 days)
-            lower_date = datetime.now() - timedelta(days=4)
-            upper_date = datetime.now() - timedelta(days=3)
-            
-            backups_to_delete = ddb.list_backups(TableName=item, TimeRangeLowerBound=datetime(lower_date.year, lower_date.month, lower_date.day)) #, TimeRangeUpperBound=datetime(upper_date.year, upper_date.month, upper_date.day))
+            # upper_date = datetime.utcnow() - timedelta(minutes=15)
+            lower_date = datetime.utcnow() - timedelta(days=4)
+
+            backups_to_delete = ddb.list_backups(TableName=item, TimeRangeLowerBound=datetime(lower_date.year, lower_date.month, lower_date.day))#, TimeRangeUpperBound=datetime(upper_date.year, upper_date.month, upper_date.day))
             deletion_backup_count=len(backups_to_delete['BackupSummaries'])
-            print('Total backup count in recent days:',deletion_backup_count)
 
-            delete_upper_date = datetime.now() - timedelta(days=4)
-            delete_lower_date = datetime.now() - timedelta(days=3)
+            delete_upper_date = datetime.utcnow() - timedelta(minutes=15)
+            delete_lower_date = datetime.utcnow() - timedelta(days=4)
 
-            # TimeRangeLowerBound is the release of Amazon DynamoDB Backup and Restore - Nov 29, 2017
-            response = ddb.list_backups(TableName=item, TimeRangeLowerBound=datetime(delete_lower_date.year, delete_lower_date.month, delete_lower_date.day)) #, TimeRangeUpperBound=datetime(delete_upper_date.year, delete_upper_date.month, delete_upper_date.day))
-            
-            #check whether latest backup count is more than two before removing the old backup
-            if deletion_backup_count>0:
-                if 'LastEvaluatedBackupArn' in response:
-                    last_eval_bucket_arn = response['LastEvaluatedBackupArn']
-                else:
-                    last_eval_bucket_arn = ''
-                
-                while (last_eval_bucket_arn != ''):
-                    for record in response['BackupSummaries']:
-                        backup_arn = record['BackupArn']
-                        ddb.delete_backup(BackupArn=backup_arn)
-                        print(backupName, 'has deleted this backup:', backup_arn)
+            response = ddb.list_backups(TableName=item, TimeRangeUpperBound=delete_upper_date)
 
-                    response = ddb.list_backups(TableName=item, TimeRangeLowerBound=datetime(delete_lower_date.year, delete_lower_date.month, delete_lower_date.day), ExclusiveStartBackupArn=last_eval_bucket_arn) #, TimeRangeUpperBound=datetime(delete_upper_date.year, delete_upper_date.month, delete_upper_date.day)
-                    if 'LastEvaluatedBackupArn' in response:
-                        last_eval_bucket_arn = response['LastEvaluatedBackupArn']
-                    else:
-                        last_eval_bucket_arn = ''
-                        print ('the end')
+            if deletion_backup_count > 0:
+                for record in response['BackupSummaries']:
+                    backup_arn = record['BackupArn']
+                    ddb.delete_backup(BackupArn=backup_arn)
+                    print(backup_name, 'has deleted this backup:', backup_arn)
+
+                response = ddb.list_backups(TableName=item, TimeRangeLowerBound=datetime(delete_lower_date.year, delete_lower_date.month, delete_lower_date.day)) #, TimeRangeUpperBound=datetime(delete_upper_date.year, delete_upper_date.month, delete_upper_date.day)
+
             else:
-                print ('Backups that meet deletion criteria have been deleted')
+                print('Backups that meet deletion criteria have been deleted')
 
     except  ClientError as e:
         print(e)
