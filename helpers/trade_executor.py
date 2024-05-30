368ab0e1087b3af5a1422dbd6f8e98b50c97f6b8
cm3 <cfmiller1397@gmail.com>
2024-05-30 16:28:53 -0400

    proper contracts

--- helpers/trade_executor.py
+++ helpers/trade_executor.py
@@ -50,7 +50,11 @@ def execute_new_trades(data,table, lambda_signifier):
             is_valid = True
 
         if is_valid:
-            all_trades = row['trade_details1wk']
+            try: 
+                row['trade_details'] = ast.literal_eval(row['trade_details1wk'])
+            except:
+                continue
+            all_trades = row['trade_details']
             trades = pd.DataFrame.from_dict(all_trades)
             if len(trades) == 0:
                 continue
